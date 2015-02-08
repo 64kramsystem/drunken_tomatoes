c231d436f0a3ecfae05559512a9a28d6731b54bc
Saverio Miroddi <saverio.pub2@gmail.com>
2015-02-08 21:02:47 +0100

    added mysql2 driver, sample database.yml, structure.sql.

---
+++ db/structure.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 5.6.23, for linux-glibc2.5 (x86_64)
+--
+-- Host: localhost    Database: drunken_tomatoes
+-- ------------------------------------------------------
+-- Server version	5.6.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `directors`
+--
+
+DROP TABLE IF EXISTS `directors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directors` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `url_path` varchar(128) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url_path` (`url_path`)
+) ENGINE=InnoDB AUTO_INCREMENT=1692 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `directors_movies`
+--
+
+DROP TABLE IF EXISTS `directors_movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directors_movies` (
+  `director_id` mediumint(8) unsigned NOT NULL,
+  `movie_id` int(10) unsigned NOT NULL,
+  UNIQUE KEY `director_id` (`director_id`,`movie_id`),
+  UNIQUE KEY `movie_id` (`movie_id`,`director_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `genres`
+--
+
+DROP TABLE IF EXISTS `genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genres` (
+  `id` tinyint(3) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `genres_movies`
+--
+
+DROP TABLE IF EXISTS `genres_movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genres_movies` (
+  `genre_id` tinyint(3) unsigned NOT NULL,
+  `movie_id` int(10) unsigned NOT NULL,
+  UNIQUE KEY `genre_id` (`genre_id`,`movie_id`),
+  UNIQUE KEY `movie_id` (`movie_id`,`genre_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `movies`
+--
+
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movies` (
+  `id` int(10) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `synopsis` text NOT NULL,
+  `rating` tinyint(3) unsigned NOT NULL,
+  `poster` blob NOT NULL,
+  `year` year(4) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `url_path` varchar(128) NOT NULL,
+  `watched` tinyint(3) unsigned NOT NULL,
+  `ignore` tinyint(3) unsigned NOT NULL,
+  `watchlist` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url_path` (`url_path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reviews` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `link` mediumtext NOT NULL,
+  `movie_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `movie_id` (`movie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53085 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `trailers`
+--
+
+DROP TABLE IF EXISTS `trailers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trailers` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `link` mediumtext NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `movie_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `movie_id` (`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-08 21:01:35
