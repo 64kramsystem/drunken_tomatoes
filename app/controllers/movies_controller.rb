1f73095de130fec778c35e065399493ff1a5031f
Saverio Miroddi <saverio.pub2@gmail.com>
2015-02-12 17:05:17 +0100

    Added pagination.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,22 @@
 class MoviesController < ApplicationController
 
+  MOVIES_PER_PAGE = 30
+
   def index
-  	@movies = Movie.all[ 0, 30 ]
+    @movies = Movie.page( param_page ).per( MOVIES_PER_PAGE )
+  end
+
+  private
+
+  def param_page
+    case params[ :page ]
+    when nil
+      1
+    when /\A\d+\Z/
+      params[ :page ].to_i
+    else
+      raise "Invalid page parameter!"
+    end
   end
 
 end
