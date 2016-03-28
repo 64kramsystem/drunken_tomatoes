dc340bc5eb01b2137fa122469c0b946711578fda
Saverio Miroddi <saverio.pub2@gmail.com>
2016-03-28 12:08:41 +0200

    Added mininum rating filter.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,19 +9,23 @@ class MoviesController < ApplicationController
 
   ANNOTATIONS = %w[watched watchlist ignore]
 
+  RATINGS = 10.step(100, 10).to_a
+
   def index
     page           = int_param( :page ) || 1
     @genre_id      = int_param( :genre_id )
     @sorting_field = whitelist_param( :sorting_field, SORT_MAPPING.values )
+    @min_rating    = int_param( :min_rating )
 
     @movies = Movie.page( page ).per( MOVIES_PER_PAGE )
     @movies = @movies.joins( :genres ).where( genres: { id: @genre_id } ) if @genre_id
+    @movies = @movies.where( 'rating >= ?', @min_rating ) if @min_rating
     @movies = @movies.order( "#{ @sorting_field } DESC" ) if @sorting_field
 
     @genres = Genre.all
-
     @sort_mapping = SORT_MAPPING.clone
     @annotations = ANNOTATIONS
+    @ratings = RATINGS
   end
 
   def update
